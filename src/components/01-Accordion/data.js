ed0d794004f55bcc4dc70011f44bd45dfd701cc9
bolaji87 <sakabolaji77@gmail.com>
2025-01-15 14:09:37 +0100

    feat: pass data to Accordion component

---
+++ src/components/01-Accordion/data.js
@@ -0,0 +1,26 @@
+export const faqData = [
+  {
+    id: 1,
+    question: "Where are these chairs assembled?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    id: 2,
+    question: "How long do I have to return my chair?",
+    answer:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    id: 3,
+    question: "Do you ship to countries outside the EU?",
+    answer:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+  {
+    id: 4,
+    question: "Can i create an Accordion component with different framework?",
+    answer:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
