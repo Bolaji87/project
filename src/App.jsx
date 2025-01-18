feb7ca72821547beed8a3d6aeed5b773757b3543
bolaji87 <sakabolaji77@gmail.com>
2025-01-18 22:32:52 +0100

    feat: call starRating component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 // import AccordionIndex from "./components/01-Accordion/AccordionIndex";
-import RandomColor from "./components/02-random-color/RandomColor";
+// import RandomColor from "./components/02-random-color/RandomColor";
+import StarRating from "./components/03-star-rating/StarRating";
 
 const App = () => {
   return (
     <div>
       {/* <AccordionIndex /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating noOfStars={10} />
     </div>
   );
 };
